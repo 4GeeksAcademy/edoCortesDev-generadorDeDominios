e3ad53e1b206df1cfdf7ca20bba2e31f2011e165
edoCortesDev <edo.cortes.rojas@gmail.com>
2025-04-09 08:57:06 +0000

    codigo js terminado

--- src/app.js
+++ src/app.js
@@ -7,5 +7,15 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  const pronombres = ["el", "ese", "aquel"];
+  const adjetivos = ["valiente", "inteligente", "fuerte"];
+  const sustantivos = ["guerrero", "científico", "atleta"];
+  
+  for (let i = 0; i < pronombres.length; i++) {
+      for (let j = 0; j < adjetivos.length; j++) {
+          for (let k = 0; k < sustantivos.length; k++) {
+              console.log(`${pronombres[i]}${adjetivos[j]}${sustantivos[k]}.com`);
+          }
+      }
+  }  
 };
